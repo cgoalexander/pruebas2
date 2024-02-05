4f880f61d6f60fb0d47beb6e7c35425e04d8706d
Edinson Alexander Solano <cgoalexander731@gmail.com>
2024-02-05 07:42:35 -0500

    Se procede a actualizar los datos del archivo de graficas (pie.png)

--- charts/charts.py
+++ charts/charts.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 def generate_pie_chart():
     labels = ["A","B","C"]
-    values = [200, 34, 120]
+    values = [800, 900, 1200]
     
     fig, ax = plt.subplots()
     ax.pie(values, labels = labels)
